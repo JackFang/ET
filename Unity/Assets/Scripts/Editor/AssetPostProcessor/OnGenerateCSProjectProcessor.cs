b8d76a8ca02fcb9e5f6a6f008ab5bc681992b216
tanghai <egametang@gmail.com>
2022-06-29 17:34:54 +0800

    把服务端合并到Unity中去

---
+++ Unity/Assets/Scripts/Editor/AssetPostProcessor/OnGenerateCSProjectProcessor.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.Xml;
+using System.IO;
+using System.Text;
+
+namespace ET
+{
+    public class OnGenerateCSProjectProcessor : AssetPostprocessor
+    {
+        public static string OnGeneratedCSProject(string path, string content)
+        {
+            
+            if (path.EndsWith("Client.Hotfix.csproj"))
+            {
+                content =  content.Replace("<Compile Include=\"Assets\\Client\\Hotfix\\Empty.cs\" />", string.Empty);
+                content =  content.Replace("<None Include=\"Assets\\Client\\Hotfix\\Client.Hotfix.asmdef\" />", string.Empty);
+            }
+            
+            if (path.EndsWith("Client.HotfixView.csproj"))
+            {
+                content =  content.Replace("<Compile Include=\"Assets\\Client\\HotfixView\\Empty.cs\" />", string.Empty);
+                content =  content.Replace("<None Include=\"Assets\\Client\\HotfixView\\Client.HotfixView.asmdef\" />", string.Empty);
+            }
+            
+            if (path.EndsWith("Client.Model.csproj"))
+            {
+                content =  content.Replace("<Compile Include=\"Assets\\Client\\Model\\Empty.cs\" />", string.Empty);
+                content =  content.Replace("<None Include=\"Assets\\Client\\Model\\Client.Model.asmdef\" />", string.Empty);
+            }
+            
+            if (path.EndsWith("Client.ModelView.csproj"))
+            {
+                content =  content.Replace("<Compile Include=\"Assets\\ModelView\\Empty.cs\" />", string.Empty);
+                content =  content.Replace("<None Include=\"Assets\\Client\\ModelView\\Client.ModelView.asmdef\" />", string.Empty);
+            }
+            
+            if (path.EndsWith("Server.Hotfix.csproj"))
+            {
+                content =  content.Replace("<Compile Include=\"Assets\\Server\\Hotfix\\Empty.cs\" />", string.Empty);
+                content =  content.Replace("<None Include=\"Assets\\Server\\Hotfix\\Server.Hotfix.asmdef\" />", string.Empty);
+            }
+            
+            if (path.EndsWith("Server.Model.csproj"))
+            {
+                content =  content.Replace("<Compile Include=\"Assets\\Server\\Model\\Empty.cs\" />", string.Empty);
+                content =  content.Replace("<None Include=\"Assets\\Server\\Model\\Server.Model.asmdef\" />", string.Empty);
+            }
+            
+            if (path.EndsWith("Client.Hotfix.csproj"))
+            {
+                return GenerateCustomProject(path, content, @"Client\Hotfix\**\*.cs");
+            }
+
+            if (path.EndsWith("Client.HotfixView.csproj"))
+            {
+                return GenerateCustomProject(path, content, @"Client\HotfixView\**\*.cs");
+            }
+
+            if (path.EndsWith("Client.Model.csproj"))
+            {
+                return GenerateCustomProject(path, content, @"Client\Model\**\*.cs");
+            }
+
+            if (path.EndsWith("Client.ModelView.csproj"))
+            {
+                return GenerateCustomProject(path, content, @"Client\ModelView\**\*.cs");
+            }
+            
+            if (path.EndsWith("Server.Model.csproj"))
+            {
+                return GenerateCustomProject(path, content, @"Server\Model\**\*.cs", @"Client\Model\**\*.cs");
+            }
+            
+            if (path.EndsWith("Server.Hotfix.csproj"))
+            {
+                return GenerateCustomProject(path, content, @"Server\Hotfix\**\*.cs", @"Client\Hotfix\**\*.cs");
+            }
+            
+            return content;
+        }
+
+        private static string GenerateCustomProject(string path, string content, params string[] codesPath)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(content);
+
+            var newDoc = doc.Clone() as XmlDocument;
+
+            var rootNode = newDoc.GetElementsByTagName("Project")[0];
+
+            var itemGroup = newDoc.CreateElement("ItemGroup", newDoc.DocumentElement.NamespaceURI);
+            foreach (string s in codesPath)
+            {
+                var compile = newDoc.CreateElement("Compile", newDoc.DocumentElement.NamespaceURI);
+                compile.SetAttribute("Include", s);
+                itemGroup.AppendChild(compile);
+            }
+
+            
+
+            var projectReference = newDoc.CreateElement("ProjectReference", newDoc.DocumentElement.NamespaceURI);
+            projectReference.SetAttribute("Include", @"..\Share\Analyzer\Share.Analyzer.csproj");
+            projectReference.SetAttribute("OutputItemType", @"Analyzer");
+            projectReference.SetAttribute("ReferenceOutputAssembly", @"false");
+
+            var project = newDoc.CreateElement("Project", newDoc.DocumentElement.NamespaceURI);
+            project.InnerText = @"{d1f2986b-b296-4a2d-8f12-be9f470014c3}";
+            projectReference.AppendChild(project);
+
+            var name = newDoc.CreateElement("Name", newDoc.DocumentElement.NamespaceURI);
+            name.InnerText = "Analyzer";
+            projectReference.AppendChild(project);
+
+            itemGroup.AppendChild(projectReference);
+
+            rootNode.AppendChild(itemGroup);
+
+            using (StringWriter sw = new StringWriter())
+            {
+
+                using (XmlTextWriter tx = new XmlTextWriter(sw))
+                {
+                    tx.Formatting = Formatting.Indented;
+                    newDoc.WriteTo(tx);
+                    tx.Flush();
+                    return sw.GetStringBuilder().ToString();
+                }
+            }
+        }
+    }
+}
